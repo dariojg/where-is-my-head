b3cf5620a4473a9a6f61fdb320e645160e8a9b87
alexbaico <alexbaico@hotmail.com>
2021-05-11 00:15:50 -0300

    Added trees to trees.tres. Added Global script. Added WanderController node for wander behaviour on enemies

--- Characters/Enemies/ChineseCR.gd
+++ Characters/Enemies/ChineseCR.gd
@@ -9,6 +9,8 @@ onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 onready var attackRange = $AttackRange
+onready var playerDetectionZone = $PlayerDetectionZone
+onready var wanderController = $WanderController
 
 enum {
 	IDLE,
@@ -21,26 +23,38 @@ var state = IDLE
 var speed = 100
 var velocity = Vector2.ZERO
 
-onready var PlayerDetectionZone = $PlayerDetectionZone
+var knockback = Vector2.ZERO
+var knockback_vector = Vector2.ZERO
+var friction = 200
 
 func _ready():
 	animationTree.active = true
 	$AttackArea.damage = self.damage
+	state = pick_random_state([IDLE, WANDER])
+	
 func _physics_process(delta):
 	set_animation_state()
+	
+	knockback = knockback.move_toward(Vector2.ZERO, friction * delta)
+	knockback = move_and_slide(knockback)
 	
 	match state:
 		IDLE:
 			velocity = Vector2.ZERO
-			seek_player()
-			
+			seek_and_wander()
+		
 		WANDER:
-			pass
+			seek_and_wander()
+			velocity = global_position.direction_to(wanderController.target_position)			
+
+			if global_position.distance_to(wanderController.target_position) <= 5:
+				pick_random_state_and_reset_wander()
+			move_and_slide(velocity * speed)
 			
 		CHASE:
-			var player = PlayerDetectionZone.player
+			var player = playerDetectionZone.player
 			if player != null:
-				velocity = (player.global_position - global_position).normalized()
+				velocity = global_position.direction_to(player.global_position)
 				if attackRange.is_colliding():
 					state = ATTACK
 				else:
@@ -68,8 +82,21 @@ func get_hurted(damage_received):
 		queue_free() #Actually animation.travel(death)
 
 func seek_player():
-	if PlayerDetectionZone.can_see_player():
+	if playerDetectionZone.can_see_player():
 		state = CHASE
+
+func seek_and_wander():
+	seek_player()
+	if wanderController.timer_time_left() == 0:
+		pick_random_state_and_reset_wander()
+
+func pick_random_state_and_reset_wander():
+		state = pick_random_state([IDLE, WANDER])
+		wanderController.start_wander_time(rand_range(1,3))
+
+func pick_random_state(state_list):
+	state_list.shuffle()
+	return state_list.pop_front()
 
 func attack_animation_finished():
 	state = CHASE
---
+++ Characters/Enemies/WanderController.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+onready var start_position = global_position
+onready var target_position = global_position
+
+onready var timer = $Timer
+
+export var wander_range = 20
+
+func _ready():
+	update_target_position()
+
+func update_target_position():
+	#moves a random range around start position
+	target_position = start_position + Vector2(rand_range(-wander_range, wander_range), rand_range(-wander_range, wander_range))
+	
+func _on_Timer_timeout():
+	update_target_position()
+
+func start_wander_time(duration):
+	timer.start(duration)
+
+func timer_time_left():
+	return timer.time_left
