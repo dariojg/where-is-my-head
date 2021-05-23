d6aaf410d7520f978a05b46d8331335e5b5d1048
alexbaico <alexbaico@hotmail.com>
2021-05-23 00:47:00 -0300

    added hit animation

--- Characters/AttackArea.gd
+++ Characters/AttackArea.gd
@@ -3,7 +3,11 @@ extends Position2D
 var damage = 0
 var knockback_vector = Vector2.ZERO
 
+onready var HitAnimationPlayer = $HitAnimation/AnimationPlayer
+
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("hurtable"):
 		body.get_hurted(damage)
 		body.knockback = knockback_vector * 150
+		HitAnimationPlayer.stop()
+		HitAnimationPlayer.play("hit")
--- Proyectiles/Arrow.gd
+++ Proyectiles/Arrow.gd
@@ -1,12 +1,13 @@
 extends Area2D
 
 var launched = false
-var velocity = Vector2(0, 0)
+export var velocity = Vector2(0, 0)
 var speed = 500
 var time_to_live = 10 #10 segs
 var knockback_power = 80
 var knockback_direction = Vector2.ZERO
-
+onready var HitAnimationPlayer = $HitAnimation/AnimationPlayer
+onready var Sprite = $Sprite
 
 var damage = 20
 
@@ -22,9 +23,14 @@ func launch(initial_velocity : Vector2):
 	launched = true
 	knockback_direction = initial_velocity
 	velocity = initial_velocity * speed
+	Sprite.visible = true
 
 func _on_Arrow_body_entered(body):
+	velocity = Vector2.ZERO
 	if body.is_in_group("hurtable"):
 		body.get_hurted(damage)
 		body.knockback = knockback_direction * knockback_power
-	queue_free()
+		HitAnimationPlayer.stop()
+		HitAnimationPlayer.play("hit")
+	else:
+		queue_free()
